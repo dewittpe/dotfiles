d26093472602cdd44be67c87ba7820b3f995569a
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-01-17 22:29:20 -0700

    add the githubforks.sh script for adding urls for the forks on github to a repo

---
+++ githubforks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# githubforks.sh
+#
+# use:
+#
+#   githubforks.sh [name]
+#
+# If [name] is omitted the url for the "origin" will be used to determine the
+# user/repo which is used to get all the ssh_urls for all the forks.
+#
+# Assumptions: the [name] remote is on github.  urls for the [name] and the
+# forks will be git@github.com:user/repo.git
+#
+# **git alias**
+#
+# copy this script, or soft link it, to
+#
+#  ~/.githubforks.sh
+#
+# Add an alias to git
+#
+# git config --global alias.githubforks '!sh ~/.githubforks.sh'
+
+if [ -z $1 ]
+then
+  echo looking for the url of the \'origin\'
+  name=origin
+else
+  name=$1
+fi
+
+userrepo=($(git remote get-url --all $name | gawk -v FS=':' '{print $2}'))
+
+URLS=($(curl https://api.github.com/repos/$userrepo/forks | grep "ssh_url" | gawk -v FS='"' '{print $4}'))
+
+for i in "${URLS[@]}"
+do
+  git remote add $(echo "$i"  | gawk -v FS=':|/' '{print $2}') $i
+done
+
+git fetch --verbose --all
