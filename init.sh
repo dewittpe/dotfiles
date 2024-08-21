f9db4cc7c954b581d8d600be35c38b82b8cfd241
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2024-08-21 09:06:05 -0600

    refactor init.sh

--- init.sh
+++ init.sh
@@ -4,38 +4,24 @@ dir=$HOME/dotfiles
 bkupdir=$HOME/dotfiles.bkup
 mkdir -p $bkupdir
 
+# Neovim
+mkdir -p $HOME/.config/
+ln -s $dir/config/nvim $HOME/.config/nvim
+
 # if a .file exists, mv to the backup directory.
 # create a softlink either way
 
-# terminator
-if [ -f $HOME/.config/terminator/config ]; then
-  mv $HOME/.config/terminator/config $bkupdir/terminator.config
-fi
-
-mkdir -p $HOME/.config/terminator
-ln -s $dir/terminator.conf $HOME/.config/terminator/config
-
-mkdir -p $HOME/.R
-ln -s $dir/R-Makevars $HOME/.R/Makevars
-
 # All other files can be looped over 
-for file in bashrc bash_aliases dircolors git-prompt.sh Rprofile tmux.conf gitmessage
-do
-    filename=$(basename $file)
-            echo $file
-    if [[ "$filename" != "$(basename $0)" ]]; then
-        echo "Creating link for .$filename"
-        # Move existing dotfile to $backup
-        mv $HOME/.$filename $bkupdir/
-        # Create symlink
-        ln -s $dir/$filename $HOME/.$filename
-    fi
-done 
+#for file in bashrc dircolors git-prompt.sh Rprofile tmux.conf gitmessage
+#do
+    #filename=$(basename $file)
+            #echo $file
+    #if [[ "$filename" != "$(basename $0)" ]]; then
+        #echo "Creating link for .$filename"
+        ## Move existing dotfile to $backup
+        #mv $HOME/.$filename $bkupdir/
+        ## Create symlink
+        #ln -s $dir/$filename $HOME/.$filename
+    #fi
+#done 
 
-# Neovim
-# mkdir -p $HOME/.config/nvim
-#ln -s $dir/vim/bundle $HOME/.config/nvim/bundle
-#ln -s $dir/init.vim $HOME/.config/nvim/init.vim
-
-# Pull in submodules
-#git submodule init && git submodule update
