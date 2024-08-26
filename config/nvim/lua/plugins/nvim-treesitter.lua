9fbaf4ae53f179f6829d98e3f2d858db5aea6674
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2024-08-26 10:47:41 -0600

    update config for nvim-treesitter

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,17 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function ()
-      require("nvim-treesitter.configs").setup({
-	sync_install = true,
-	ensure_installed = {
-	  "r",
-	  "markdown",
-	  "markdown_inline",
-	  "rnoweb",
-	},
-      })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  run = ":TSUpdate",
+  config = function ()
+    require("nvim-treesitter.configs").setup({
+      sync_install = true,
+      ensure_installed = {
+        "r",
+        "markdown",
+        "markdown_inline",
+        "rnoweb",
+        "yaml"
+      },
+      highlight = { enable = true },
+    })
+  end,
 }
