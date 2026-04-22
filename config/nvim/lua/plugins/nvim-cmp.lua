a0370cffdc2e5d094080ec287adee939ccad60a7
Peter E. DeWitt <peter.dewitt@cuanschutz.edu>
2026-04-22 09:45:41 -0600

    update R.vim and dependencies for Nvim > 0.12

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,30 +1,26 @@
 return {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-	sources = {{ name = "cmp_r" }},
-	mapping = cmp.mapping.preset.insert({
-	  ['<CR>'] = cmp.mapping.confirm({ select = false }),
-	  -- During auto-completion, press <Tab> to select the next item.
-	  ['<Tab>'] = cmp.mapping(function(fallback)
-	    if cmp.visible() then
-	      cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-	    -- elseif has_words_before() then
-	    --  cmp.complete()
-	    else
-	      fallback()
-	    end
-	  end, { 'i', 's' }),
-	  ['<S-Tab>'] = cmp.mapping(function(fallback)
-	    if cmp.visible() then
-	      cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-	    else
-	      fallback()
-	    end
-	  end, { 'i', 's' }),
-	}),
-      })
-      require("cmp_r").setup({ })
-    end,
-  }
+  "hrsh7th/nvim-cmp",
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+    })
+  end,
+}
--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,18 +1,17 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = 'master',
-  run = ":TSUpdate",
-  config = function ()
-    require("nvim-treesitter.configs").setup({
-      sync_install = true,
-      ensure_installed = {
-        "r",
-        "markdown",
-        "markdown_inline",
-        "rnoweb",
-        "yaml"
-      },
-      highlight = { enable = true },
-    })
-  end,
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local langs = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex", "csv" }
+        require("nvim-treesitter").install(langs)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = langs,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
